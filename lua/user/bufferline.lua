97a64e8a5cf550298a1270d4e713ea8247be1a60
vinvit <vitalik00pr@gmail.com>
2022-09-04 23:56:51 +0300

    fix bufferline icon deprecated

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -13,7 +13,10 @@ bufferline.setup {
     -- NOTE: this plugin is designed with this icon in mind,
     -- and so changing this is NOT recommended, this is intended
     -- as an escape hatch for people who cannot bear it for whatever reason
-    indicator_icon = "▎",
+    indicator = {
+      style = 'icon',
+      icon = "▎",
+    },
     buffer_close_icon = "",
     -- buffer_close_icon = '',
     modified_icon = "●",
@@ -34,8 +37,8 @@ bufferline.setup {
     max_name_length = 30,
     max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
     tab_size = 21,
-    diagnostics = false, -- | "nvim_lsp" | "coc",
-    diagnostics_update_in_insert = false,
+    diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
+    diagnostics_update_in_insert = true,
     -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
     --   return "("..count..")"
     -- end,
