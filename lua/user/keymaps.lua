f454dc9c34308605be37d4eddea61fecede6c3c2
vinvit <vitalik00pr@gmail.com>
2022-09-04 22:50:21 +0300

    install toggleterm

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -15,10 +15,7 @@ vim.g.maplocalleader = " "
 --   insert_mode = "i",
 --   visual_mode = "v",
 --   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
-
--- Normal --
+--   term_mode = "t", command_mode = "c", Normal --
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
@@ -65,10 +62,10 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+--[[ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts) ]]
+--[[ keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts) ]]
+--[[ keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts) ]]
+--[[ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts) ]]
 
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
