f085048a2cecb7bc8b2b6e911a9d2d4f4003dc3f
vinvit <vitalik00pr@gmail.com>
2022-09-04 21:13:27 +0300

    add gitsign highlight

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,6 +1,6 @@
 local fn = vim.fn
 
--- Automatically install packer
+-- Automatically install packer 
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
   PACKER_BOOTSTRAP = fn.system {
@@ -77,6 +77,9 @@ return packer.startup(function(use)
     run = ":TSUpdate",
   }
   use 'JoosepAlviste/nvim-ts-context-commentstring'
+
+  -- Git
+  use "lewis6991/gitsigns.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
