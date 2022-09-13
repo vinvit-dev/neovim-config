537fccecfef906131ffced55d3c4c76eb7520aa4
vinvit <vitalik00pr@gmail.com>
2022-09-14 00:01:19 +0300

    add maximize current buffer extension

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,6 +94,9 @@ return packer.startup(function(use)
 
   -- phpcs
   use("praem90/nvim-phpcsf")
+
+  use("caenrique/nvim-maximize-window-toggle");
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
